0065dd1bba7156f7e37822643d1e5a14821423a1
Bai <batuhanakkurt000@gmail.com>
2024-01-15 19:19:02 +0000

    fd

--- src/jsler/Yerler.tsx
+++ src/jsler/Yerler.tsx
@@ -1,4 +1,7 @@
+/*kütüphane alma*/
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+/*compenent alma*/
 import '../cssler/Yerler.css';
 import kart from '../res/kart.png';
 
@@ -6,9 +9,15 @@ function Yerler() {
   return (
     <div className="Yerler">
       <header className="Yerler-header">
+
         <a href="https://github.com/Baturax?tab=repositories" className="repoyagonderis">Bu site ayrıca açık kaynaktır</a>
-        <h1 className='bvaslik'>dd</h1>
-        <img src={kart} className='kartfoto' />
+
+        <h1 className="baslik">Seç:</h1>
+
+        <div className="karticin">
+        <img src={kart} className="kartfoto" />
+        <h3 className="kartadi">Karta Bak</h3>
+        </div>
       </header>
     </div>
   );
